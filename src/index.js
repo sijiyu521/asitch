557e65afbb455c1fae93666a88b8483b9dd890ca
Munei <huangb8555@outlook.com>
2026-08-19 17:48:49 +0800

    Revert "用 C / SDL2 重写引擎，替换 JavaScript 实现"

---
+++ src/index.js
@@ -0,0 +1,6 @@
+export { Engine } from './engine.js';
+export { Scene } from './scene.js';
+export { Sprite } from './sprite.js';
+export { Camera } from './camera.js';
+export { AssetManager } from './asset-manager.js';
+export { Input } from './input.js';
